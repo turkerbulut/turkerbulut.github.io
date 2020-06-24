8ddbfefd73b45fb4a63f613b3c117f2762ae621c
turkerbulut <tturkerbulut@gmail.com>
2020-06-24 14:55:12 -0400

    try again

---
+++ _pages/recipe.md
@@ -0,0 +1,17 @@
+---
+layout: page
+permalink: recipe
+---
+
+## Recipe title
+
+# Ingredients
+
+1. apples
+2. bananas
+3. water
+
+# Instructions
+
+- Do step 1
+- Do step 2
---
+++ assets/script.js
@@ -0,0 +1,23 @@
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function CopyLink() {
+  copyTextToClipboard(location.href);
+}
